94a639dc0d50cd715d6a5cdfcf7392fb7fe21fe3
Andrzej Kowalski <jedreksmith@poczta.onet.pl>
2024-05-12 21:48:21 +0200

    Add project files.

---
+++ Ex_w_2_l_4_2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex_w_2_l_4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+     
+            string First = "11a";
+
+            string Second = "22b";
+
+            string Third = "33c";
+
+            Console.WriteLine(Third + Second + First);
+
+        }
+    }
+
+
+  }
+   
+
